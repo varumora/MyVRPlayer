022e29b4bc715369c5bf656b26ac259c2e7829a8
alvaro <alvaro@droiders.com>
2016-09-07 15:17:32 +0200

    imágenes del derecho

--- player/src/main/res/raw/photo_fragment_shader.glsl
+++ player/src/main/res/raw/photo_fragment_shader.glsl
@@ -3,7 +3,7 @@ varying vec2 vTextureCoord;
 uniform sampler2D sTexture;
 
 void main() {
-    vec4 color = texture2D(sTexture, vec2(vTextureCoord.x,vTextureCoord.y+1.0));
+    vec4 color = texture2D(sTexture, vec2(vTextureCoord.x,1.0-(vTextureCoord.y+1.0)));
     vec4 color2 = vec4(color.x,color.y,color.z,1.0);
     gl_FragColor = color2;
 }
